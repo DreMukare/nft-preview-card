e684482e6701e4ea34575a14f50f2e6e648ec808
JudgeFudge19 <dremukare@gmail.com>
2021-11-27 23:38:39 +0300

    feat: achieved basic card layout <need to resize for smaller viewports and fix active state

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Card from './components/Card';
+import './styles/app.scss';
+
+function App() {
+	return (
+		<div className='App'>
+			<Card />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,41 @@
+import '../styles/card.scss';
+import equilibrium from '../images/image-equilibrium.jpg';
+import iconEthereum from '../images/icon-ethereum.svg';
+import iconView from '../images/icon-view.svg';
+import iconClock from '../images/icon-clock.svg';
+import avatar from '../images/image-avatar.png';
+
+const Card = () => {
+	return (
+		<div className='card'>
+			<section className='card__image'>
+				<img src={iconView} alt='nft preview' />
+			</section>
+			<section className='card__content'>
+				<h2>Equilibrium #3429</h2>
+				<p>Our Equilibrium collection promotes balance and calm.</p>
+			</section>
+			<section className='card__detail'>
+				<div className='price'>
+					<section className='image'>
+						<img src={iconEthereum} alt='Ethereum icon' />
+					</section>
+					<p>0.041ETH</p>
+				</div>
+				<div className='time'>
+					<img src={iconClock} alt='clock icon' />
+					<p>3 days left</p>
+				</div>
+			</section>
+			<hr />
+			<section className='card__attribution'>
+				<img src={avatar} alt='Creator of NFT' />
+				<p>
+					Creation of <span>Jules Wyvern</span>
+				</p>
+			</section>
+		</div>
+	);
+};
+
+export default Card;
